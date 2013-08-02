065a4464f4c355dd2ab96273c2aaa297d7f2b4e0
unknown <allisonh@allison-laptop.apollo.smartsheet.com>
2013-08-02 16:01:49 -0700

    Adding Administrative Features example

---
+++ c#/4 - AdministrativeFeatures/Admin.cs
@@ -0,0 +1,159 @@
+﻿//  Copyright 2013 Smartsheet, Inc.
+
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+
+//       http://www.apache.org/licenses/LICENSE-2.0
+
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace ManageUsers
+{
+    class ManageUsers
+    {
+        static void Main(string[] args)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            //Instantiates a couple of user objects, with parameters that can be referenced later in the script
+            //Replace the default string values with your own
+            User userA = new User { firstName = "User", lastName = "A", email = "youremail+1@gmail.com", admin = true, licensedSheetCreator = true };
+            User userB = new User { firstName = "User", lastName = "B", email = "youremail+2@gmail.com", admin = true, licensedSheetCreator = true };
+
+            //Add the users to the Team account based on their email address
+            SmartsheetRequest addUser = new SmartsheetRequest { method = "POST", callURL = "/users?sendEmail=true", contentType = "application/json" };
+            addUser.MakeRequest(js.Serialize(userA));
+            addUser.MakeRequest(js.Serialize(userB));
+
+            //This sends a confirmation email to the addresses 
+            //The users need to click the accept button in their emails to finalize the action, enabling you to make "assume user" requests to Smartsheet
+            Console.Write("Your invitations have been sent. Wait for users to accept the invitation to the team via email.\n Press any key to continue when this is complete\n");
+            Console.ReadKey();
+
+            //After the invitations have been accepted, list the org users to confirm that this has been completed
+            SmartsheetRequest listOrgUsers = new SmartsheetRequest { method = "GET", callURL = "/users", contentType = "application/json" };
+            var listUsersResponse = listOrgUsers.MakeRequest("null");
+
+            Console.Write("A full list of users on my Team account are displayed below:\n");
+            for (int i = 0; i < listUsersResponse.Length; i++)
+            {
+                Console.Write((i + 1) + ". " + listUsersResponse[i]["email"] + "\n");
+
+                //Store the user ID to the user objects, so you can easily refer to them later in the code
+                if (listUsersResponse[i]["email"] == userA.email)
+                {
+                    userA.id = listUsersResponse[i]["id"].ToString();
+                }
+                if (listUsersResponse[i]["email"] == userB.email)
+                {
+                    userB.id = listUsersResponse[i]["id"].ToString();
+                }
+            }
+
+            //Create a new sheet as yourself
+            string mySheet = "{\"name\":\"My Sheet\",\"columns\":[{\"title\":\"First Column\",\"primary\":true, \"type\":\"TEXT_NUMBER\"}]}]}";
+            SmartsheetRequest createSheet = new SmartsheetRequest { method = "POST", callURL = "/sheets", contentType = "application/json" };
+            createSheet.MakeRequest(mySheet);
+
+            //Pass "assume user" to the class to create a new sheet as User A
+            string userASheet = "{\"name\":\"User A's Sheet\",\"columns\":[{\"title\":\"First Column\",\"primary\":true, \"type\":\"TEXT_NUMBER\"}]}]}";
+            //Uri.EscapeDateString to URi-decode the email address
+            SmartsheetRequest createSheetAsUserA = new SmartsheetRequest { method = "POST", callURL = "/sheets", contentType = "application/json", assumeUser = Uri.EscapeDataString(userA.email) };
+            createSheetAsUserA.MakeRequest(userASheet);
+
+            //Pass "assume user" with different parameters to create a new sheet as User B
+            string userBSheet = "{\"name\":\"User B's Sheet\",\"columns\":[{\"title\":\"First Column\",\"primary\":true, \"type\":\"TEXT_NUMBER\"}]}]}";
+            //Uri.EscapeDateString to URi-encode the email address
+            SmartsheetRequest createSheetAsUserB = new SmartsheetRequest { method = "POST", callURL = "/sheets", contentType = "application/json", assumeUser = Uri.EscapeDataString(userB.email) };
+            createSheetAsUserB.MakeRequest(userBSheet);
+
+            Console.Write("\nNew Sheets have been created...\n");
+
+            //List all sheets owned by users in the org
+            //Confirms that the "assume user" requests were successful
+            SmartsheetRequest listOrgSheets = new SmartsheetRequest { method = "GET", callURL = "/users/sheets", contentType = "application/json" };
+            var orgSheets = listOrgSheets.MakeRequest("null");
+
+            Console.Write("\nA full list of all sheets owned by users on my Team account are displayed below: \n");
+            for (int i = 0; i < orgSheets.Length; i++)
+            {
+                Console.Write((i + 1) + ". " + orgSheets[i]["name"] + ", owned by " + orgSheets[i]["owner"] + "\n");
+            }
+
+            //To delete a user, access their user ID
+            //Optionally transfer their owned sheets to another user on the account - in this case, User B
+            //Optionally remove the user's access from sharing all sheets owned by users on the Team/Enterprise account
+            string userToDelete = userA.id;
+            SmartsheetRequest deleteUser = new SmartsheetRequest { method = "DELETE", callURL = "/user/" + userToDelete + "?transferTo=" + userB.id + "&removeFromSharing=true", contentType = "application/json" };
+            var deleteUserResponse = deleteUser.MakeRequest("null");
+
+            Console.Write("\nUser with ID " + userToDelete + " has been deleted.\n");
+            Console.Read();
+        }
+    }
+
+    class User
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public bool admin { get; set; }
+        public bool licensedSheetCreator { get; set; }
+        public string id { get; set; }
+    }
+
+    class SmartsheetRequest
+    {
+        public string assumeUser { get; set; }
+        public string method { get; set; }
+        public string callURL { get; set; }
+        public string contentType { get; set; }
+
+        JavaScriptSerializer js = new JavaScriptSerializer();
+
+        //Replace the text with your Smartsheet API token, generated in-app
+        string token = "insert token here";
+        string baseURL = "https://api.smartsheet.com/1.1";
+
+        public dynamic MakeRequest(string json)
+        {
+            WebRequest request = WebRequest.Create(baseURL + callURL);
+            request.ContentType = contentType;
+            request.Method = method;
+            request.Headers.Add("Authorization: Bearer " + token);
+
+            //Only add the assumeUser header if the argument was passed
+            if (String.IsNullOrEmpty(assumeUser) == false)
+            {
+                request.Headers.Add("Assume-User: " + assumeUser);
+            }
+
+            if (json != "null")
+            {
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(json);
+                }
+            }
+            Stream responseStream = request.GetResponse().GetResponseStream();
+            StreamReader readStream = new StreamReader(responseStream);
+            var jsonResponse = js.Deserialize<dynamic>(readStream.ReadToEnd());
+
+            return jsonResponse;
+        }
+    }
+}
---
+++ c#/4 - AdministrativeFeatures/README.md
@@ -0,0 +1,90 @@
+Managing Users & Sheets on Team/Enterprise Accounts (C#)
+===
+See our <b>Hellosmartsheet</b>, <b>SheetStructure</b> and <b>Attachments</b> scripts for a hands-on introduction to the Smartsheet API. The fourth in the series, this semi-interactive script walks you through the Smartsheet API administrative calls and capabilities by managing team members (creating, updating and deleting), executing operations on behalf of other team members, and listing all sheets in a team account.
+
+Smartsheet API
+---
+Familiarize yourself with the Smartsheet API. For information on the Smartsheet API, please see the [Smartsheet Developer Portal](http://smartsheet.com/developers).
+
+.NET Framework
+---
+This script was built on .NET Framework 4.5. To set the target framework of the project in VS Express 2012, click Project → Console Application Properties → Application, and make a selection from the Target Framework drop-down list. Make sure it is not set to use the Client Profile.
+
+References and Using Directives
+---
+In the Smartsheet API, request body data is expected to be in JSON, and the response body data is returned as JSON. This program uses the built-in JavaScriptSerializer class to serialize and deserialize JSON. Third-party libraries are available for this, but the built-in option is rather straight forward and easy to work with.
+
+In order to use the JavaScriptSerializer class, add a reference to the program. In VS Express 2012, click Project → Add Reference → Framework. Select System.Web.Extensions to use JavaScriptSerializer in the application.
+
+The following namespaces are used in the program to save time when typing out methods:
+
+System.IO - for the [StreamReader](http://msdn.microsoft.com/en-us/library/system.io.streamreader.aspx) and [StreamWriter](http://msdn.microsoft.com/en-us/library/system.io.streamwriter.aspx) classes
+
+System.Net - for the [WebRequest](http://msdn.microsoft.com/en-us/library/system.net.webrequest.aspx) class
+
+System.Web.Script.Serialization - for the [JavaScriptSerializer](http://msdn.microsoft.com/en-us/library/system.web.script.serialization.javascriptserializer.aspx) class
+
+Usage
+---
+Smartsheet has several subscription options, or plans, ranging from Basic to Enterprise - see the [Smartsheet pricing page](http://smartsheet.com/pricing) for more information. Administrative features are only available in multi-user plans (Team and Enterprise).
+
+To execute any admin operations, you must have (1) a multi-user plan, and (2) System Admin privileges to that account. See this help article [http://help.smartsheet.com/customer/portal/articles/520100-user-types](http://help.smartsheet.com/customer/portal/articles/520100-user-types) to learn about Smartsheet user types.
+
+While logged in as a SysAdmin of a Team or Enterprise account, generate a Smartsheet API access token and insert it into the Smartsheet Request class at the bottom of the script:
+
+        string token = "insert token here";
+
+
+You'll also notice that the email addresses passed to the User class contain default values. Both Gmail and Hotmail offer a useful "plus addressing" feature making it possible to create multiple email addresses that are all associated with the same account. Feel free to use this method to test the script with your own Gmail or Hotmail address, or replace the default values with the actual email addresses associated with the users you want to add to your Team/Enterprise account.
+
+            User userA = new User { firstName = "User", lastName = "A", email = "youremail+1@gmail.com", admin = true, licensedSheetCreator = true };
+            User userB = new User { firstName = "User", lastName = "B", email = "youremail+2@gmail.com", admin = true, licensedSheetCreator = true };
+
+
+When the script runs, You should receive two emails at youremail@gmail.com inviting User A and User B to join your Team account. Accept the invitation emails before continuing - this is a required step; without it, the invited users will not become associated with your Team account based on Smartsheet's opt-in model.
+
+
+Code
+---
+This walkthrough highlights only some parts of the code. For the full code, please see the complete <b>Admin.cs</b> script.
+
+The goal of this walkthrough is to describe how to take advantage of the Smartsheet API administrative features.
+
+When the script first runs, it will pause so that you can accept the invitation emails before continuing (as described above). Press any key when this step is complete.
+
+            SmartsheetRequest addUser = new SmartsheetRequest { method = "POST", callURL = "/users?sendEmail=true", contentType = "application/json" };
+            addUser.MakeRequest(js.Serialize(userA));
+            addUser.MakeRequest(js.Serialize(userB));
+
+            Console.Write("Your invitations have been sent. Wait for users to accept the invitation to the team via email.\n Press any key to continue when this is complete\n");
+            Console.ReadKey();
+
+Fetch the list of all members on your Team/Enterprise account - including the ones you just added:
+
+            SmartsheetRequest listOrgUsers = new SmartsheetRequest { method = "GET", callURL = "/users", contentType = "application/json" };
+            var listUsersResponse = listOrgUsers.MakeRequest("null");
+
+Next, let's create sheets on behalf of our newly added users. This operation takes advantage of the "Assume-User" feature which allows SysAdmins on Team/Enterprise accounts to execute commands on behalf of other licensed users. There are numerous business scenarios (create, update, etc.) when this is extremely useful, but it should be used with care.  Make sure that the email address of the user whose identity we are assuming is URL-encoded (in this case, using Uri.EscapeDataString):
+
+            string userASheet = "{\"name\":\"User A's Sheet\",\"columns\":[{\"title\":\"First Column\",\"primary\":true, \"type\":\"TEXT_NUMBER\"}]}]}";
+            SmartsheetRequest createSheetAsUserA = new SmartsheetRequest { method = "POST", callURL = "/sheets", contentType = "application/json", assumeUser = Uri.EscapeDataString(userA.email) };
+            createSheetAsUserA.MakeRequest(userASheet);
+	
+Now fetch the list of all sheets in your team account, including the ones we just created, regardless of whether you have been shared to them. This operation is only available to System Administrators:
+
+            SmartsheetRequest listOrgSheets = new SmartsheetRequest { method = "GET", callURL = "/users/sheets", contentType = "application/json" };
+            var orgSheets = listOrgSheets.MakeRequest("null");
+	
+If you render the result, you will see that although you executed the commands, the sheets are owned by the users on whose behalf the sheets were created.
+
+Finally, remove a user from your team account. You can optionally choose to (1) have the user's owned sheets transferred to another licensed user on the Team/Enterprise account, and (2) remove the user from sharing all items owned by others on the Team/Enterprise account:
+
+            string userToDelete = userA.id;
+            SmartsheetRequest deleteUser = new SmartsheetRequest { method = "DELETE", callURL = "/user/" + userToDelete + "?transferTo=" + userB.id + "&removeFromSharing=true", contentType = "application/json" };
+            var deleteUserResponse = deleteUser.MakeRequest("null");
+
+If you refresh the list of users, you should see that the one you just removed is gone. If you refresh the list of sheets, you should now see that the sheet(s) previously owned by the removed user are now owned by the one to whom you transferred the sheet(s).
+	
+Congratulations!  You just completed your fourth Smartsheet API C# walkthrough. We encourage you to play with the script, change it around, and enhance it to get better acquainted with the Smartsheet API. Ping us at api@smartsheet.com with any questions or suggestions.
+
+The Smartsheet Platform team. 
